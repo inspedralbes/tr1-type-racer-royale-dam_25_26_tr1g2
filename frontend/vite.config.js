05cb54d9cff8585effd0211d928e945566d5e084
elperera <a24arnpergan@inspedralbes.cat>
2025-10-29 10:15:19 +0100

    solució de l'error amb el vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // ----------------------------------------------------------------
+  // SOLUCIÓ PER A LIVE SHARE
+  server: {
+    // Estableix 'host: true' per enllaçar-se a totes les interfícies (0.0.0.0).
+    // Això és essencial perquè Live Share o qualsevol altra eina de túnel
+    // pugui exposar el servidor de desenvolupament als convidats.
+    host: true,
+  }
+  // ----------------------------------------------------------------
 })
