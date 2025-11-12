bced6c3f78183ffe4413e1dd39ce4b457b877661
Mxrta22 <a24marharfon@inspedralbes.cat>
2025-11-12 09:47:37 +0100

    barra lateral movil

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,47 @@
 <template>
   <v-app>
-    <!-- Header condicional basado en autenticación -->
+    <!-- 1. Menú Lateral (v-navigation-drawer) para Móviles -->
+    <v-navigation-drawer 
+      v-model="drawer" 
+      app 
+      temporary
+      color="#212121"
+      dark
+    >
+      <v-list nav dense>
+        <!-- Ítems del Menú Lateral -->
+        <v-list-item link to="/" @click="drawer = false">
+          <v-list-item-title class="font-weight-medium">
+            <v-icon left>mdi-home</v-icon> Inicio
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item link to="/crearrutina" @click="drawer = false">
+          <v-list-item-title class="font-weight-medium">
+            <v-icon left>mdi-dumbbell</v-icon> Crear Rutina
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item link to="/ayuda" @click="drawer = false">
+          <v-list-item-title class="font-weight-medium">
+            <v-icon left>mdi-help-circle-outline</v-icon> Ayuda
+          </v-list-item-title>
+        </v-list-item>
+        
+        <v-divider class="my-2"></v-divider>
+        
+        <v-list-item link to="/profile" color="green" @click="drawer = false">
+          <v-list-item-title class="font-weight-bold green--text text--lighten-1">
+            <v-icon left color="green">mdi-account</v-icon> PROFILE
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- 2. Header condicional y escucha de evento -->
     <AppHeader v-if="!isAuthenticated" />
-    <AppHeader2 v-else />
+    <!-- Cuando el usuario está autenticado, AppHeader2 emite 'toggleDrawer' -->
+    <AppHeader2 v-else @toggleDrawer="drawer = !drawer" />
 
     <!-- Contenido de cada página -->
     <v-main>
@@ -15,10 +54,14 @@
 </template>
 
 <script setup>
+import { ref } from 'vue' // Importar ref para el estado del drawer
 import AppHeader from '@/components/AppHeader.vue'
 import AppHeader2 from '@/components/AppHeader2.vue'
 import AppFooter from '@/components/AppFooter.vue'
 import { useAuth } from '@/composables/useAuth'
 
 const { isAuthenticated } = useAuth()
+
+// 💡 Estado para controlar la apertura del menú lateral (Navigation Drawer)
+const drawer = ref(false) 
 </script>
